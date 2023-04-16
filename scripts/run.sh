891c35997da9edd154dbc3bf929053569b51ae55
why-not-try-calmer <mrnycticorax@gmail.com>
2023-04-16 15:11:08 +0200

    iso_to_docker script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
-docker run \
-    -v $(pwd)/app:/opt/app \
-    -p 8000:8000 \
-    --env-file=$(pwd)/.env \
-    pop-ready:latest
+docker build . -t pop-docker:latest
+docker run -it pop-docker:latest bash
--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-docker run \
-    -v $(pwd)/app:/opt/app \
-    -v $(pwd)/tests:/opt/tests \
-    -p 8000:8000 \
-    --env-file=$(pwd)/.env \
-    pop-ready:latest bash -c "cd /opt && python3 -m pytest tests/tests.py -v -s" 
